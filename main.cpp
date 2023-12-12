3e87d640137af424b7eb696e5e848f5cb7109707
kastor <47624693+qkrtjsrbs315@users.noreply.github.com>
2023-12-12 18:40:25 +0900

    Fix: JSON parsing

--- main.cpp
+++ main.cpp
@@ -8,12 +8,33 @@
 #include <time.h>
 
 using namespace cv;
+
 // 콜백 함수 정의
 size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
 {
     size_t realsize = size * nmemb;
     char *data = (char *)userp;
+
+    // 수신된 데이터의 끝에 널 문자를 추가하기 위해 메모리를 할당합니다.
+    char *new_data = (char *)realloc(data, realsize + 1);
+
+    if (new_data == NULL)
+    {
+        fprintf(stderr, "응답 데이터에 대한 메모리 할당 실패.\n");
+        return 0; // libcurl에 실패를 알리는 값
+    }
+
+    data = new_data;
+
+    // 수신된 데이터를 복사합니다.
     memcpy(data, contents, realsize);
+
+    // 문자열에 널 문자를 추가합니다.
+    data[realsize] = '\0';
+
+    // 사용자 포인터를 업데이트합니다.
+    *((char **)userp) = data;
+
     return realsize;
 }
 
@@ -52,10 +73,10 @@ char *getCityFromIpInfo()
     // Extract city from the IP response
     json_error_t error;
     json_t *root = json_loads(ipResponseBuffer, 0, &error);
-    
+
     if (!root)
     {
-        fprintf(stderr, "JSON parsing failed: %s\n, ip_response_buffer: %s\n", error.text,ipResponseBuffer);
+        fprintf(stderr, "JSON parsing failed: %s\n, ip_response_buffer: %s\n", error.text, ipResponseBuffer);
         return NULL;
     }
 
@@ -70,7 +91,10 @@ char *getCityFromIpInfo()
     // libcurl 정리
     curl_global_cleanup();
 
-    return strdup(city);
+    char *result = strdup(city);
+    json_decref(root);
+
+    return result;
 }
 
 // OpenWeatherMap API에서 날씨 정보를 가져오는 함수
@@ -111,8 +135,6 @@ char *getWeatherInfo(const char *city)
         json_t *root = json_loads(response_buffer, 0, &error);
         if (root)
         {
-            //response_buffer[res] = "\0";
-
             json_t *weather = json_object_get(root, "weather");
             if (json_array_size(weather) > 0)
             {
@@ -123,21 +145,21 @@ char *getWeatherInfo(const char *city)
                 {
                     const char *mainString = json_string_value(mainValue);
                     printf("now weather: %s\n", mainString);
-                    
-  		    char *result = strdup(mainString);
+
+                    char *result = strdup(mainString);
                     json_decref(root);
                     curl_easy_cleanup(curl);
                     curl_global_cleanup();
-    
+
                     return result;
-                 }
+                }
             }
 
-             json_decref(root);
+            json_decref(root);
         }
         else
         {
-            fprintf(stderr, "JSON parsing failed: %s\n Weather response buffer: %s\n", error.text,response_buffer);
+            fprintf(stderr, "JSON parsing failed: %s\n Weather response buffer: %s\n", error.text, response_buffer);
         }
     }
 
@@ -147,64 +169,73 @@ char *getWeatherInfo(const char *city)
     curl_global_cleanup();
 
     return NULL;
-
 }
-void ImageShow(const char weather[]){
-   char path[256];
-   sprintf(path,"./img/%s.png",weather);
-   Mat img = imread(path);
-   if (img.empty()){
-      printf("Error: Could not open or read the image file\n");
-      return;
-   }
-   //check already open window
-   if(!cvGetWindowHandle("Weather")){
-     namedWindow("Weather",WINDOW_AUTOSIZE);
 
-   }  
-   
-   imshow("Weather",img);
-   waitKey(1);
+// 이미지를 표시하는 함수
+void ImageShow(const char weather[])
+{
+    char path[256];
+    sprintf(path, "./img/%s.png", weather);
+    Mat img = imread(path);
+    if (img.empty())
+    {
+        printf("Error: Could not open or read the image file\n");
+        return;
+    }
 
+    // 창이 이미 열려 있는지 확인
+    if (!cvGetWindowHandle("Weather"))
+    {
+        // 창이 없으면 새로 엽니다.
+        namedWindow("Weather", WINDOW_AUTOSIZE);
+    }
 
+    imshow("Weather", img);
+    waitKey(1); // 1밀리초 동안 대기 (창 업데이트를 처리하기 위해 필요)
 
-
+    // 창을 활성화한 상태에서 어떤 입력이 없어도 이미지가 업데이트되도록 하기 위해
+    // waitKey 함수를 호출합니다. waitKey 함수는 사용자 입력을 대기하지만, 여기에서는
+    // 이미지 업데이트를 위한 목적으로 사용하고 있습니다.
 }
-//check O'clock
-bool isHourChecked(){
-   time_t rawtime;
-   struct tm *timeinfo;
-  
-   time(&rawtime);
-   timeinfo = localtime(&rawtime);
-   
-   return (timeinfo -> tm_min == 0 && timeinfo->tm_sec == 0);
-   //return (timeinfo -> tm_sec == 0);
+
+// 정각인지 확인하는 함수
+bool isHourChecked()
+{
+    time_t rawtime;
+    struct tm *timeinfo;
+
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+
+    return (timeinfo->tm_min == 0 && timeinfo->tm_sec == 0);
 }
 
 int main(void)
 {
-    bool initialExecution = true;   
-    while(true){
-         //if time is o'clock
-         if(initialExecution || isHourChecked()){
+    bool initialExecution = true;
+    while (true)
+    {
+        // if time is o'clock
+        if (initialExecution || isHourChecked())
+        {
             char *city = getCityFromIpInfo();
-     
+
             if (city)
             {
-               printf("now City: %s\n", city);
-               char *weather = getWeatherInfo(city);
-               printf("main function weather value : %s\n",weather);
-               ImageShow(weather);
-            
-            
-               //사용이 끝난 도시 정보 메모리 해제
-               free(weather);
-               free(city);
+                printf("now City: %s\n", city);
+                char *weather = getWeatherInfo(city);
+                printf("main function weather value : %s\n", weather);
+                ImageShow(weather);
+
+                // 사용이 끝난 도시 정보 메모리 해제
+                free(weather);
+                free(city);
+
+                initialExecution = false; // 초기 실행 후 플래그 업데이트
             }
             else
             {
-              printf("Can't fetch the City Info.\n");
+                printf("Can't fetch the City Info.\n");
             }
         }
     }
